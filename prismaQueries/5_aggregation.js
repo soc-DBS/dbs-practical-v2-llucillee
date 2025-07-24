10b004778cf9cb1d31af198f2d8b0ce0d8e0d84f
llucillee <ngtllucille@gmail.com>
2025-07-24 11:01:55 +0800

    aggregation

---
+++ prismaQueries/5_aggregation.js
@@ -0,0 +1,104 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+const util = require('util');
+
+
+function getMeanCourseFee() {
+    return prisma.course.aggregate({
+        _avg: {
+            crseFee: true,
+        }
+    })
+}
+
+
+/** Section A */
+
+function getNumberOfFullTimeStaff() {
+    return prisma.staff.aggregate({
+        _count: {
+            staffNo: true
+        },
+        where: {
+            typeOfEmployment: 'FT'
+        }
+    })
+}
+
+/** Section B */
+
+function getTotalAllowanceOfStaffByGrade() {
+    return prisma.staff.groupBy({
+        by: ['grade'],
+        where: {
+            grade: {
+                not: {
+                    startsWith: 'S'
+                }
+            },
+            allowance: {
+                not: null
+            }
+        },
+        _sum: {
+            allowance: true
+        },
+        orderBy: {
+            grade: 'desc'
+        }
+    });
+}
+
+
+
+function getTotalPayAndNoOfStaffByDeptWithHighTotal() {
+    return prisma.staff.groupBy({
+    by: ['deptCode'],
+    where: {
+      deptCode: {
+        not: 'DPO'
+      }
+    },
+    _sum: {
+      pay: true,
+      allowance: true
+    },
+    _count: {
+      staffNo: true
+    }
+    });
+}
+
+/** Using Raw Query */
+
+
+function getAvgLabFeeWithRawQuery() {
+    return prisma.$queryRaw`SELECT AVG(COALESCE(lab_fee, 0)) AS "Mean Lab Fee" FROM course;`
+}
+
+
+async function main(argument) {
+    let results;
+    switch (argument) {
+        case 'getMeanCourseFee':
+            results = await getMeanCourseFee();
+            break;
+        case 'getNumberOfFullTimeStaff':
+            results = await getNumberOfFullTimeStaff();
+            break;
+        case 'getTotalAllowanceOfStaffByGrade':
+            results = await getTotalAllowanceOfStaffByGrade();
+            break;                        
+        case 'getTotalPayAndNoOfStaffByDeptWithHighTotal':
+            results = await getTotalPayAndNoOfStaffByDeptWithHighTotal();
+            break;            
+        case 'getAvgLabFeeWithRawQuery':
+            results = await getAvgLabFeeWithRawQuery()
+            break;            
+        default:
+            console.log('Invalid argument');
+    }
+    results && console.log(util.inspect(results, { showHidden: false, depth: null, colors: true }));
+}
+
+main(process.argv[2]);
